efba5a3407c4ad56e7c5cde1377fc30af618ccc3
Graeme Robinson <1robig68@solent.ac.uk>
2018-04-17 14:28:03 +0100

    Updates
    changes to CSS and JS

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,24 +1,48 @@
-$(document).ready(function(){
-    var btt = $('.back-to-top'); /*identify class, same as on HTML file*/
+/*Old JS commented out for future reference*/
 
-    btt.on('click', function(e){ /*scrolling animation*/
-        $('html, body').animate({
+/*$(document).ready(function(){
+    var btn = $('.back-to-top'); /*identify class, same as on HTML file*/
+
+/*    btn.on('click', function(e){ /*scrolling animation*/
+      /*  $('html, body').animate({
             scrollTop: 0
         }, 1000);
         e.preventDefault(); /*Keeps button from blinking when funtion is activated*/
-    });
+ /*  });/*
 
     $(window).on('scroll', function(){ /*set height*/
-        var self = $(this), 
+  /*      var self = $(this), 
         height = self.height(), 
         top = self.scrollTop();
 
         if(top > height) { /*CSS makes button invisible by default, this IF statement allows it to appear after a certain height mark on HTML file*/
-            if(!btt.is(':visible')){
-                btt.show();
+  /*          if(!btn.is(':visible')){
+                btn.addClass('is-hidden');
             }
         } else{
-            btt.hide();
+            btn.removeClass('is-hidden');
         }
     });
-});
+});*/
+
+if ($('#back-to-top').length) {
+  var scrollTrigger = 100, // px
+    backToTop = function() {
+      var scrollTop = $(window).scrollTop();
+      if (scrollTop > scrollTrigger) {
+        $('#back-to-top').addClass('show');
+      } else {
+        $('#back-to-top').removeClass('show');
+      }
+    };
+  backToTop();
+  $(window).on('scroll', function() {
+    backToTop();
+  });
+  $('#back-to-top').on('click', function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+  });
+}
