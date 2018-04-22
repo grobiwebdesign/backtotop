7461b0106411e93dab50935d2fd69d3677d192b4
Graeme Robinson <1robig68@solent.ac.uk>
2018-04-22 10:38:20 +0100

    Updates
    span changed to button, css3 fade and cursor no longer appears on hidden
    button

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,48 +1,15 @@
-/*Old JS commented out for future reference*/
 
-/*$(document).ready(function(){
-    var btn = $('.back-to-top'); /*identify class, same as on HTML file*/
-
-/*    btn.on('click', function(e){ /*scrolling animation*/
-      /*  $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-        e.preventDefault(); /*Keeps button from blinking when funtion is activated*/
- /*  });/*
-
-    $(window).on('scroll', function(){ /*set height*/
-  /*      var self = $(this), 
-        height = self.height(), 
-        top = self.scrollTop();
-
-        if(top > height) { /*CSS makes button invisible by default, this IF statement allows it to appear after a certain height mark on HTML file*/
-  /*          if(!btn.is(':visible')){
-                btn.addClass('is-hidden');
-            }
-        } else{
-            btn.removeClass('is-hidden');
-        }
-    });
-});*/
-
-if ($('#back-to-top').length) {
-  var scrollTrigger = 100, // px
-    backToTop = function() {
-      var scrollTop = $(window).scrollTop();
-      if (scrollTop > scrollTrigger) {
-        $('#back-to-top').addClass('show');
-      } else {
-        $('#back-to-top').removeClass('show');
-      }
-    };
-  backToTop();
-  $(window).on('scroll', function() {
-    backToTop();
-  });
-  $('#back-to-top').on('click', function(e) {
-    e.preventDefault();
-    $('html,body').animate({
-      scrollTop: 0
-    }, 1000);
-  });
-}
+$(document).ready(function(){ 
+    $(window).scroll(function(){ /*set height*/
+        if ($(this).scrollTop() > 100) { /*identify class, same as on HTML file*/
+            $('#backtotop').fadeIn(); 
+        } else { 
+            $('#backtotop').fadeOut(); 
+        } 
+    }); 
+    $('#backtotop').click(function(){ /*scrolling animation*/
+        $("html, body").animate({ scrollTop: 0 }, 1000); 
+		e.preventDefault(); /*Keeps button from blinking when funtion is activated*/
+        return false; 
+    }); 
+});
